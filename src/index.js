3286380a30efaeef6036125c2427f8fc5059736c
Anvesh Karra <anveshkarra1234@gmail.com>
2023-04-24 16:20:41 +0530

    Added codes

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connect = require('./config/database');
+const app = express();
+
+const TweetRepository = require('./repository/tweet_repository');
+const Comment = require('./models/comment');
+app.listen(3000 , async () => {
+    console.log('server started');
+    await connect();
+    console.log('Mongo db connected');
+    // const tweet = await Tweet.create({
+    //     content: 'Third tweet',
+    // });
+    // const tweets = await Tweet.find({userEmail : 'a@b.com'});
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.getWithComments('64465c9ecd6c3e4acbf31b7c');
+    console.log(tweet);
+});
