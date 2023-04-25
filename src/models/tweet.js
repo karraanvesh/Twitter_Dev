0c3ab2d5d416efd4c12c9f41db8532c0476c700b
Anvesh Karra <anveshkarra1234@gmail.com>
2023-04-25 14:42:11 +0530

    Added codes

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -18,7 +18,13 @@ const tweetSchema = new mongoose.Schema({
 
 tweetSchema.virtual('contentWithEmail').get(function process() {
     return `${this.content} \nCreated by: ${this.userEmail}`;
-})
+});
+
+tweetSchema.pre('save', function(next) {
+    console.log('Inside a hook');
+    this.content = this.content + '....';
+    next();
+});
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
