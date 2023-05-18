1dd4ce8d35349fc2d5f8093fe32c66e1e9ab941c
Anvesh Karra <anveshkarra1234@gmail.com>
2023-05-18 15:23:58 +0530

    Added toggle like api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import { createTweet } from '../../controllers/tweet_controller.js';
+import { toggleLike } from '../../controllers/like_controller.js';
 
 const router = express.Router();
 
 router.post('/tweets', createTweet);
+
+router.post('/likes/toggle',toggleLike);
 
 export default router;
