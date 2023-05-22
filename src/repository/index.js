2eefee5e4ea64ee98c781af3597f63fa3133b4fc
Anvesh Karra <anveshkarra1234@gmail.com>
2023-05-22 14:52:24 +0530

    Added get tweet with nested comments api

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,10 +2,12 @@ import TweetRepository from './tweet_repository.js';
 import HashtagRepository from './hashtag_repository.js';
 import LikeRepository from './like_repository.js';
 import UserRepository from './user_repository.js';
+import CommentRepository from './comment_repository.js';
 
 export {
     TweetRepository,
     HashtagRepository,
     LikeRepository,
-    UserRepository
+    UserRepository,
+    CommentRepository
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 
-import { createTweet } from '../../controllers/tweet_controller.js';
+import { createTweet , getTweet} from '../../controllers/tweet_controller.js';
 import { toggleLike } from '../../controllers/like_controller.js';
+import { createComment } from '../../controllers/comment_controller.js';
 
 const router = express.Router();
 
 router.post('/tweets', createTweet);
+router.get('/tweets/:id', getTweet);
 
 router.post('/likes/toggle',toggleLike);
+
+router.post('/comments', createComment);
 
 export default router;
